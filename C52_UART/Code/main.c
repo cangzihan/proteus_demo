b1d0d8def85db329be2fdbcafafb3cceedd595f0
Knoll_Lenovo <cangzihan@gmail.com>
2023-03-13 20:48:09 +0800

    Update C52 UART Demo

--- C52_UART/Code/main.c
+++ C52_UART/Code/main.c
@@ -5,47 +5,47 @@
 #include"uart.h"
 
 /****************************************************************************************
-*     串口通信实验(收发)
+*     UART Demo(Send / Receive)
 ****************************************************************************************/
 extern unsigned char rcv_buff, rcv_flag;
 
 void display_initial(void)
 {
-	LCD_Init();
-	gotoxy(1,5);     // Set cursor position
-	LCD_Write_String("C52 UART");
-	gotoxy(2,1);     // Set cursor position
-	LCD_Write_String("Send:");
-	gotoxy(3,1);     // Set cursor position
-	LCD_Write_String("Rcv:");;
+    LCD_Init();
+    gotoxy(1,5);     // Set cursor position
+    LCD_Write_String("C52 UART");
+    gotoxy(2,1);     // Set cursor position
+    LCD_Write_String("Send:");
+    gotoxy(3,1);     // Set cursor position
+    LCD_Write_String("Rcv:");;
 }
 
 void system_initial(void)
 {
-	UART_Init();
+    UART_Init();
 }
 
 void main()
 {
-	unsigned char key, send_byte;
-  system_initial();
-	display_initial();
+    unsigned char key, send_byte;
+    system_initial();
+    display_initial();
 	
-	while(1)
-	{
-		key = switchs();
-		if(key)
-		{
-			send_byte = key;
-			gotoxy(2,6);
-			LCD_Write_DATA(send_byte+'0');\
-			UART_Send_Byte(send_byte);
-		}
-		if(rcv_flag)
-		{
-			rcv_flag = 0;
-			gotoxy(3,5);
-			LCD_Write_DATA(rcv_buff+'0');
-		}
-	}
+    while(1)
+    {
+      key = switchs();
+      if(key)
+      {
+          send_byte = key;
+          gotoxy(2,6);
+          LCD_Write_DATA(send_byte+'0');\
+          UART_Send_Byte(send_byte);
+      }
+      if(rcv_flag)
+      {
+          rcv_flag = 0;
+          gotoxy(3,5);
+          LCD_Write_DATA(rcv_buff+'0');
+      }
+    }
 }
--- C52_UART/Code/system/uart.c
+++ C52_UART/Code/system/uart.c
@@ -5,28 +5,28 @@ unsigned char rcv_buff, rcv_flag;
 
 void UART_Init(void)
 {
-	SCON = 0x50;                                //串口方式1，允许接收
-  TMOD= 0x20;                                 //定时器1工作方式2
-  TH1 = 0xF3;                                 //波特率2400、数据位8、停止位1
-  TL1 = 0xF3; 
-  TR1 = 1;                                    //开启定时器1
-  EA=1;                                       //开总中断
-  ES=1;                                       //开串中断
+    SCON = 0x50;                                //UART1, Enable Receive
+    TMOD= 0x20;                                 //UART1, Work Mode2
+    TH1 = 0xF3;                                 //Baud 2400, Data Bit8, Stop Bit1
+    TL1 = 0xF3; 
+    TR1 = 1;                                    //Open Timer 1
+    EA=1;                                       //Enable Interrupt
+    ES=1;                                       //Enable UART Interrupt
 }
 
 void UART_Send_Byte(unsigned char send_byte)
 {
-	ES=0;                                //进入发送数据时先关闭串行中断
-  SBUF=send_byte;                      //将数据原样发出
-  while(!TI);                          //等待数据发完
-  TI=0;
-  ES=1;                                //退出再开串行中断
+    ES=0;                                //Before Send, Disable UART Interrupt
+    SBUF=send_byte;                      //Send Data
+    while(!TI);                          //Sent?
+    TI=0;
+    ES=1;                                //After Exit, Enable UART Interrupt
 }
 
 
-void serial() interrupt 4                         //串行中断函数
+void serial() interrupt 4                         //UART Interrupt Function
 {    
-	rcv_buff=SBUF;                                  //收取数据
-  rcv_flag=1;                                     //标志复位
-  RI=0;
+    rcv_buff=SBUF;                                  //Receive Data
+    rcv_flag=1;                                     //Reset Flag
+    RI=0;
 }
